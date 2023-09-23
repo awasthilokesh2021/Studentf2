fb4fc55a9122b9484692efcc004e56a820fbf917
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-09-23 14:45:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch("product.json")
+.then((response)=>
+  response.json())
+.then((data)=>{
+  let place = document.querySelector('#data-json')
+  var out = "";
+  for(var k of data){
+    out += `
+       <tr>
+        <td>${k.id}</td>
+        <td>${k.first_name}</td>
+        <td>${k.last_name}</td>
+        <td>${k.gender}</td>
+        <td>${k.email}</td>
+        <td><img src="${k.img_src}"></td>
+       </tr>
+    `;
+}
+  place.innerHTML = out;
+})
+
+
